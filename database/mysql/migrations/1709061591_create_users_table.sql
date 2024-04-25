79c77ee02c40f2a74e5098cd9d41508cee7154f2
abbas <abbasmomeny1994@gmail.com>
2024-04-25 20:32:55 +0330

    add password col

--- database/mysql/migrations/1709061591_create_users_table.sql
+++ database/mysql/migrations/1709061591_create_users_table.sql
@@ -4,6 +4,7 @@ create table users
     id           int primary key AUTO_INCREMENT,
     name         varchar(191) not null,
     phone_number varchar(191) not null unique,
+    password     varchar(255) not null,
     created_at   datetime default current_timestamp
 );
 
