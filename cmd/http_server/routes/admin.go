b335ef85f4fc2f167653d864fc878f30db49f6ba
abbas <abbasmomeny1994@gmail.com>
2024-04-28 09:37:01 +0330

    load session repo and srv

--- cmd/http_server/routes/admin.go
+++ cmd/http_server/routes/admin.go
@@ -1,9 +1,11 @@
 package routes
 
 import (
-	"github.com/abbasfisal/ecommerce-go/cmd/http_server/handler"
-	"github.com/abbasfisal/ecommerce-go/repository"
-	"github.com/abbasfisal/ecommerce-go/services"
+	"github.com/abbasfisal/ecommerce-go/internal/admin/handlers"
+	"github.com/abbasfisal/ecommerce-go/internal/admin/repository"
+	adminAuthSrv "github.com/abbasfisal/ecommerce-go/internal/admin/service/authservice"
+	sessionRepoResolver "github.com/abbasfisal/ecommerce-go/internal/session/repository"
+	sessionSrvResolver "github.com/abbasfisal/ecommerce-go/internal/session/service"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,9 +13,13 @@ func (s ServerApis) SetAdminRoutes(r *gin.RouterGroup) {
 	// routes url => v1/admin
 	grp := r.Group("/admin")
 
-	repo := repository.NewAdmin(s.Db)
-	srv := services.NewAdminService(repo)
-	hnd := handler.NewAdminHandler(srv)
+	authRepo := repository.NewAuthenticate(s.Db)
+	authSvc := adminAuthSrv.New(authRepo)
+
+	sessionRepo := sessionRepoResolver.NewSessionRepository(s.Db)
+	sessionSrv := sessionSrvResolver.NewSessionService(sessionRepo)
+
+	hnd := handlers.NewAdminHandler(authSvc, sessionSrv)
 
 	grp.GET("/login", hnd.ShowLogin)
 	grp.POST("/login", hnd.Login)
