5e979d1487cf38e4bd794c4d7b3696eddd4eff91
abbas <abbasmomeny1994@gmail.com>
2024-04-28 09:58:51 +0330

    add last_name to user sql

--- internal/database/mysql/migrations/1709061591_create_users_table.sql
+++ internal/database/mysql/migrations/1709061591_create_users_table.sql
@@ -3,6 +3,7 @@ CREATE TABLE `users`
 (
     id           INT PRIMARY KEY AUTO_INCREMENT,
     first_name   VARCHAR(191) NOT NULL,
+    last_name    VARCHAR(191) NOT NULL,
     phone_number VARCHAR(191) NOT NULL UNIQUE,
     password     VARCHAR(255) NOT NULL,
     type         ENUM('client' , 'admin') DEFAULT 'client',
