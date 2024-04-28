e963c88ba3c90ff2adf1bb522406807b60c542e8
abbas <abbasmomeny1994@gmail.com>
2024-04-28 08:37:06 +0330

    change users table

---
+++ internal/database/mysql/migrations/1709061591_create_users_table.sql
@@ -0,0 +1,15 @@
+-- +migrate Up
+CREATE TABLE `users`
+(
+    id           INT PRIMARY KEY AUTO_INCREMENT,
+    first_name   VARCHAR(191) NOT NULL,
+    phone_number VARCHAR(191) NOT NULL UNIQUE,
+    password     VARCHAR(255) NOT NULL,
+    type         ENUM('client' , 'admin') DEFAULT 'client',
+    created_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at   DATETIME,
+    deleted_at   DATETIME
+);
+
+-- +migrate Down
+Drop Table users;
