c57bb093237fd2f2b040ae8f93171c8098e9f1eb
abbas <abbasmomeny1994@gmail.com>
2024-04-29 20:33:18 +0330

    add categories sql

---
+++ internal/database/mysql/migrations/1714410032_create_categories_table.sql
@@ -0,0 +1,15 @@
+-- +migrate Up
+CREATE TABLE categories
+(
+    id         INT PRIMARY KEY AUTO_INCREMENT,
+    title      VARCHAR(255) NOT NULL,
+    slug       VARCHAR(255) NOT NULL UNIQUE,
+    image      VARCHAR(255),
+    created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME,
+    deleted_at DATETIME
+
+);
+
+-- +migrate Down
+Drop Table categories;
