fdbc449dace74e94fd722d06503cadc7554e1309
abbas <abbasmomeny1994@gmail.com>
2024-04-29 16:45:30 +0330

    implement Login() admin authService

--- internal/admin/service/authservice/service.go
+++ internal/admin/service/authservice/service.go
@@ -2,8 +2,11 @@ package authservice
 
 import (
 	"context"
+	"errors"
 	"github.com/abbasfisal/ecommerce-go/internal/admin/contract"
 	"github.com/abbasfisal/ecommerce-go/internal/admin/transport/http/requests"
+	"github.com/abbasfisal/ecommerce-go/internal/entity"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type AuthService struct {
@@ -16,5 +19,17 @@ func New(repo contract.AuthRepo) AuthService {
 	}
 }
 
-func (s AuthService) Login(ctx context.Context, request requests.LoginRequest) {
+func (s AuthService) Login(ctx context.Context, request requests.LoginRequest) (entity.User, error) {
+
+	//check user by mobile and type = admin
+	user, err := s.repo.GetUserBy(request.PhoneNumber)
+	if err != nil {
+		return entity.User{}, err
+	}
+	//compare hashed password
+	hashCheckErr := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(request.Password))
+	if hashCheckErr != nil {
+		return entity.User{}, errors.New("record not found")
+	}
+	return user, nil
 }
