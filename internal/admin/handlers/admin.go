825fb2ef1ecedb0ebdd30c5cfda25572afe3e0f2
abbas <abbasmomeny1994@gmail.com>
2024-05-02 08:37:14 +0330

    add categorySrv to AdminHandler and implement methods StoreCategory() ,ShowCreateCategory()

--- internal/admin/handlers/admin.go
+++ internal/admin/handlers/admin.go
@@ -4,22 +4,27 @@ import (
 	"fmt"
 	"github.com/abbasfisal/ecommerce-go/internal/admin/contract"
 	"github.com/abbasfisal/ecommerce-go/internal/admin/transport/http/requests"
+	"github.com/abbasfisal/ecommerce-go/internal/admin/transport/http/template"
 	sessionContract "github.com/abbasfisal/ecommerce-go/internal/session/contract"
+	"github.com/abbasfisal/ecommerce-go/internal/util"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 	"log"
 	"net/http"
+	"os"
 )
 
 type AdminHandler struct {
-	authSrv    contract.AuthService
-	sessionSrv sessionContract.SessionService
+	authSrv     contract.AuthService
+	sessionSrv  sessionContract.SessionService
+	categorySrv contract.CategoryService
 }
 
-func NewAdminHandler(authSrv contract.AuthService, sessionSrv sessionContract.SessionService) AdminHandler {
+func NewAdminHandler(authSrv contract.AuthService, sessionSrv sessionContract.SessionService, categorySrv contract.CategoryService) AdminHandler {
 	return AdminHandler{
-		authSrv:    authSrv,
-		sessionSrv: sessionSrv,
+		authSrv:     authSrv,
+		sessionSrv:  sessionSrv,
+		categorySrv: categorySrv,
 	}
 }
 
@@ -108,4 +113,83 @@ func (h AdminHandler) Login(c *gin.Context) {
 
 	c.Redirect(http.StatusMovedPermanently, "v1/admin/dashboard")
 	return
+}
+
+func (h AdminHandler) ShowCreateCategory(c *gin.Context) {
+	fmt.Println("\n--- route categories triggered ---")
+	c.HTML(http.StatusOK, "create-category.html", nil)
+	return
+}
+
+func (h AdminHandler) StoreCategory(c *gin.Context) {
+	fmt.Println("\n---upload store category", "\n\t form posted data ", c.Request.PostForm)
+	var req requests.CreateCategoryRequest
+
+	//bind
+	if err := c.ShouldBind(&req); err != nil {
+		c.HTML(http.StatusBadRequest, "create-category.html", template.Data{
+			Message: "validation failed",
+			Error:   err.Error(),
+		})
+
+		fmt.Println("\n--- bind err : ", err)
+		return
+	}
+	fmt.Println("\n\t --- request values", req, "\t", req.Status)
+
+	//todo: use validator pkg
+
+	imageFile, err := c.FormFile("image")
+	if err != nil {
+		c.HTML(http.StatusBadRequest, "create-category.html", template.Data{
+			Message:         "get image failed",
+			Error:           err.Error(),
+			ValidationError: nil,
+			StatusCode:      0,
+			Data:            nil,
+			Meta:            nil,
+		})
+		return
+	}
+	fmt.Println("\n--image header", imageFile.Header)
+
+	imageMustStorePath := util.GenerateFilename(imageFile.Filename)
+	fmt.Println("\n--imageMustStorePath", imageMustStorePath)
+
+	if err := c.SaveUploadedFile(imageFile, imageMustStorePath); err != nil {
+		fmt.Println("\n--- store image error :", template.Data{
+			Message: "store image into disk failed",
+			Error:   err.Error(),
+		})
+		return
+	}
+	fmt.Println("\n\t ---- req", &req, c)
+	category, catErr := h.categorySrv.StoreCategory(c, &req, imageMustStorePath)
+
+	if catErr != nil {
+		fmt.Println("\n\t--- error from repository ", catErr)
+		err := os.Remove(imageMustStorePath)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		c.HTML(http.StatusBadRequest, "create-category.html", template.Data{
+			Message:         "failed to create a record to db",
+			Error:           err.Error(),
+			ValidationError: nil,
+			StatusCode:      0,
+			Data:            nil,
+			Meta:            nil,
+		})
+
+		return
+	}
+
+	c.HTML(http.StatusCreated, "create-category.html", template.Data{
+		Message: "category created successfully",
+		Data: map[string]any{
+			"category": category,
+		},
+		Meta: nil,
+	})
 }
