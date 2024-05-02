dcd36979bc70c2e50ffcfad4d1158a20f7c24080
abbas <abbasmomeny1994@gmail.com>
2024-05-02 05:28:04 +0330

    add template Data structure

---
+++ internal/admin/transport/http/template/data.go
@@ -0,0 +1,10 @@
+package template
+
+type Data struct {
+	Message         string
+	Error           string
+	ValidationError map[string]any
+	StatusCode      int
+	Data            map[string]any
+	Meta            map[string]any
+}
