b73318e246054525e09cef97a3b5f02d28b4fa4d
abbas <abbasmomeny1994@gmail.com>
2024-05-02 05:30:57 +0330

    add store and show route for category

--- cmd/http_server/routes/admin.go
+++ cmd/http_server/routes/admin.go
@@ -4,7 +4,7 @@ import (
 	"github.com/abbasfisal/ecommerce-go/internal/admin/handlers"
 	"github.com/abbasfisal/ecommerce-go/internal/admin/repository"
 	adminAuthSrv "github.com/abbasfisal/ecommerce-go/internal/admin/service/authservice"
-	"github.com/abbasfisal/ecommerce-go/internal/middleware"
+	"github.com/abbasfisal/ecommerce-go/internal/admin/service/categoryservice"
 	sessionRepoResolver "github.com/abbasfisal/ecommerce-go/internal/session/repository"
 	sessionSrvResolver "github.com/abbasfisal/ecommerce-go/internal/session/service"
 	"github.com/gin-gonic/gin"
@@ -19,19 +19,25 @@ func (s ServerApis) SetAdminRoutes(r *gin.Engine) {
 	sessionRepo := sessionRepoResolver.NewSessionRepository(s.Db)
 	sessionSrv := sessionSrvResolver.NewSessionService(sessionRepo)
 
-	hnd := handlers.NewAdminHandler(authSvc, sessionSrv)
+	categoryRepo := repository.NewCategoryRepository(s.Db)
+	categorySrv := categoryservice.NewService(categoryRepo)
+
+	hnd := handlers.NewAdminHandler(authSvc, sessionSrv, categorySrv)
 
 	//=>login-admin
 	r.GET("/login-admin", hnd.ShowLogin)
 	r.POST("/login-admin", hnd.Login)
 
-	grp := r.Group("v1/admin", middleware.IsAdmin)
+	grp := r.Group("v1/admin") //todo:add middleware IsAdmin
 	{
 		//group route =>v1/admin/dashboard
 		grp.GET("/dashboard", func(c *gin.Context) {
 			c.HTML(http.StatusOK, "dashboard.html", nil)
 			return
 		})
+
+		grp.GET("/categories", hnd.ShowCreateCategory)
+		grp.POST("/categories", hnd.StoreCategory)
 
 	}
 }
