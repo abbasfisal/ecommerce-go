428a231cdfa4886f6811891dc509e4dbc9cee93e
abbas <abbasmomeny1994@gmail.com>
2024-05-06 07:30:29 +0330

    add HasMany relation Category with Product

--- internal/entity/category.go
+++ internal/entity/category.go
@@ -4,8 +4,9 @@ import "gorm.io/gorm"
 
 type Category struct {
 	gorm.Model
-	Title  string
-	Slug   string
-	Image  string
-	Status bool
+	Products []Product `gorm:"foreignKey:CategoryID"`
+	Title    string
+	Slug     string
+	Image    string
+	Status   bool
 }
