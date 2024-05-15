fd5c10a61aa1427d4eaae20de450c59c8f3d1396
abbas <abbasmomeny1994@gmail.com>
2024-05-15 12:03:09 +0330

    add CollectAndTranslateValidationErrors() and ValidateMobileNumber()

--- internal/pkg/validate.go
+++ internal/pkg/validate.go
@@ -5,6 +5,7 @@ import (
 	ut "github.com/go-playground/universal-translator"
 	"github.com/go-playground/validator/v10"
 	faTranslations "github.com/go-playground/validator/v10/translations/fa"
+	"regexp"
 )
 
 var trans ut.Translator
@@ -15,11 +16,29 @@ func NewValidate() *validator.Validate {
 	uni := ut.New(faTranslator, faTranslator)
 
 	trans, _ = uni.GetTranslator("fa")
-	validate := validator.New()
+	validate := validator.New(validator.WithRequiredStructEnabled())
+	_ = validate.RegisterValidation("mobile", ValidateMobileNumber)
+
 	_ = faTranslations.RegisterDefaultTranslations(validate, trans)
 
 	return validate
 }
 func GetTrans() ut.Translator {
 	return trans
+}
+
+// CollectAndTranslateValidationErrors will return slice of validation errors which translated to selected language
+func CollectAndTranslateValidationErrors(err error) []map[string]any {
+	var validationError []map[string]any
+	for _, err := range err.(validator.ValidationErrors) {
+		validationError = append(validationError, map[string]any{
+			err.Field(): err.Translate(GetTrans()),
+		})
+	}
+	return validationError
+}
+func ValidateMobileNumber(fl validator.FieldLevel) bool {
+	mobileNumber := fl.Field().String()
+	regex := regexp.MustCompile(`^09\d{9}$`)
+	return regex.MatchString(mobileNumber)
 }
