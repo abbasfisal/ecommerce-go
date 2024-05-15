be876bb73af6e0ddd36e1bac9a5f6cf77dc0dfb9
abbas <abbasmomeny1994@gmail.com>
2024-05-15 12:02:17 +0330

    change to slice ValidationError

--- internal/admin/transport/http/template/data.go
+++ internal/admin/transport/http/template/data.go
@@ -3,7 +3,7 @@ package template
 type Data struct {
 	Message         string
 	Error           string
-	ValidationError map[string]any
+	ValidationError []map[string]any
 	StatusCode      int
 	Data            map[string]any
 	Meta            map[string]any
